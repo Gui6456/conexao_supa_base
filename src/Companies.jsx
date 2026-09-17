936fc2e1fce2a19d46318f54cdde991c68dd5c39
Gui6456 <guiborgia@gmail.com>
2026-09-17 17:17:52 -0300

    criação do site de consulta de tabelas de empresas e funcionarios, resta mexer nos inputs e salvar funcionarios novos

--- src/Companies.jsx
+++ src/Companies.jsx
@@ -7,13 +7,19 @@ function Companies() {
     const [companies, setCompanies] = useState([])
     const [employees, setEmployees] = useState([])
 
+    const [show_employees, setshowEmployees] = useState(false)
+    const [show_companies, setshowCompanies] = useState(true)
+    const [show_modal, setshowModal] = useState(false)
+
     async function search_all_companies (){
+
         const { error , data} = await supabase.from("companies").select()
         console.log(data)
         setCompanies(data)
     }
 
     async function search_all_employees (){
+
         const { error , data} = await supabase  
         .from("employees")  // Esse modo faz com que eu consiga acessar dentro de uma tabela, escolher o que quero mostrar, e entrar em outra tabela também
         .select(`     
@@ -28,6 +34,29 @@ function Companies() {
         setEmployees(data)
     }
 
+    async function search_employees_by_interprise (id_interprise) {
+
+        const { error , data} = await supabase.from("employees").select('*,companies(*)').eq('id_interprise', id_interprise)
+        console.log(data)
+        setEmployees(data)
+        change_view()
+    }
+
+    async function insert_employee() {
+        <></>
+    }
+
+    function change_view () {
+
+        if (show_companies == true) {
+            setshowCompanies(false)
+            setshowEmployees(true)
+        } else {
+            setshowCompanies(true)
+            setshowEmployees(false)
+        }
+    }
+
     useEffect( ()=> {
         search_all_companies()
         search_all_employees()
@@ -38,60 +67,104 @@ function Companies() {
             <div>
 
                 <div className="titulo_sub" >
-                    <h1> Empresas </h1>
-                    <p>Consulta na tabela empresas e funcionários</p>
+                    <h1> Relacionamento de Tabelas </h1>
+                    <p> Consulta na tabela empresas e funcionários </p>
                 </div>
 
-                <div>
-                    <table>
-                        <tr>
-                            <td> ID </td>
-                            <td> Name </td>
-                            <td> CNPJ </td>
-                            <td> Address </td>
-                        </tr>
+                {   // Para as empresas
 
-                        {
-                            companies.map( i => 
+                   show_companies == true ?
+
+                        <div>
+                            <table>
                                 <tr>
-                                    <td> {i.id} </td>
-                                    <td> {i.name} </td>
-                                    <td> {i.cnpj} </td>
-                                    <td> {i.address} </td>
+                                    <td> ID </td>
+                                    <td> Name </td>
+                                    <td> CNPJ </td>
+                                    <td> Address </td>
                                 </tr>
-                            )
-                        }
 
-                    </table>
+                                {
+                                    companies.map( i => 
+                                        <tr>
+                                            <td> {i.id} </td>
+                                            <td> {i.name} </td>
+                                            <td> {i.cnpj} </td>
+                                            <td> {i.address} </td>
+                                            <td> <button onClick={ ()=> search_employees_by_interprise(i.id)}> Ver funcionários </button> </td>
+                                        </tr>
+                                    )
+                                }
+                            </table>
+                        </div>
+                    :
+                        <></>
+                }
+                        
+                {
 
-                    <h2> Funcionários </h2>
+                    show_modal == true ?
+                    <div>
+                        <div onClick={()=> setshowModal(false)} className="fundo_preto" ></div>
 
-                    <table>
-                        <tr>
-                            <td> ID </td>
-                            <td> Name </td>
-                            <td> Company Name </td>
-                            <td> Company Address </td>
-                            <td> Cargo </td>
-                            <td> Contact </td>
-                        </tr>
+                        <div className="modal">
+                            <h2>Novo Funcionário</h2>
+                            <input placeholder="Nome..." />    <br/><br/>
+                            <input placeholder="Contato..." />  <br/><br/>
 
-                        {
+                            <select>   
+                                <option value="1"> Funcionário comum </option>   
+                                <option value="0"> Administrador </option>
+                            </select> <br/><br/>
 
-                            employees.map( i =>
-                                <tr>
-                                    <td> {i.id} </td>
-                                    <td> {i.name} </td> {/* Isso faz com que eu navegue dentro da tabela de companies, e faça com que busque um item dentro da companies, dentro de name, caso não ache nao retorne nada */}
-                                    <td> {i.companies ? i.companies.name : ""} </td>  
-                                    <td> {i.companies ? i.companies.address : ""} </td>
-                                    <td> {i.cargo} </td>
-                                    <td> {i.contact} </td>
-                                </tr>
-                            )
-                        }
-                    </table>
-                </div>
+                            <button onClick={insert_employee} > Salvar </button> 
 
+                        </div>
+                    </div>
+                    :
+                        <></>
+                }
+
+                    {
+                        show_employees == true ?
+
+                    <div>
+                        
+                        <h2> Funcionários </h2>
+                        <button className="botao_voltar" onClick={change_view}> Voltar </button>
+                        <button onClick={()=> setshowModal(true)} > Adicionar novo </button>
+
+                        <table>
+                            <tr>
+                                <td> ID </td>
+                                <td> Name </td>
+                                <td> Company Name </td>
+                                <td> Company Address </td>
+                                <td> Cargo </td>
+                                <td> Contact </td>
+                            </tr>
+
+                            {
+
+                                employees.map( i =>
+                                    <tr>
+                                        <td> {i.id} </td>
+                                        <td> {i.name} </td> {/* Isso faz com que eu navegue dentro da tabela de companies, e faça com que busque um item dentro da companies, dentro de name, caso não ache nao retorne nada */}
+                                        <td> {i.companies.name} </td>  
+                                        <td> {i.companies.address} </td>
+                                        <td> {i.cargo == 0 ? 'Administrador' : 'Funcionário comum'} </td>
+                                        <td> {i.contact} </td>
+                                    </tr>
+                                )
+                            }
+
+                        </table>
+                    </div>
+
+                        :
+                            <></>
+                }
+                
             </div>
         );
 }
